d7805194f5e1428531f793666f35e7b98cefab7d
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-02-22 09:57:36 -0800

    Created placeholders for initial TMGE classes/interfaces

---
+++ tmge/src/main/java/edu/uci/tmge/Player.java
@@ -0,0 +1,18 @@
+package edu.uci.tmge;
+
+public class Player {
+
+  private String name;
+
+  public Player(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ tmge/src/main/java/edu/uci/tmge/core/PlayerWriter.java
@@ -0,0 +1,4 @@
+package edu.uci.tmge.core;
+
+public class PlayerWriter {
+}

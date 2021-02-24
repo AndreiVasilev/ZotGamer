e009a4dbdd72ad9ae1d28bdca9eef7c75e783496
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-02-24 13:38:46 -0800

    Added MainView as primary scene of application

--- main/src/main/java/edu/uci/main/ApplicationMain.java
+++ main/src/main/java/edu/uci/main/ApplicationMain.java
@@ -2,16 +2,17 @@ package edu.uci.main;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public final class ApplicationMain extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    final Scene mainScene = new Scene(new AnchorPane());
-    primaryStage.setHeight(800);
-    primaryStage.setWidth(800);
+    final Scene mainScene = new Scene(new MainViewController());
+    primaryStage.setMaxHeight(575);
+    primaryStage.setMinHeight(575);
+    primaryStage.setMaxWidth(640);
+    primaryStage.setMinWidth(640);
     primaryStage.setTitle("Zot Gamer");
     primaryStage.setScene(mainScene);
     primaryStage.show();

4962b9e3131e6160506ff93f9be692130ecfa793
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-02-27 14:15:39 -0800

    Added references to FXML elements in puzzle bobble view controller

--- puzzlebobble/src/main/java/edu/uci/puzzlebobble/PuzzleBobbleViewController.java
+++ puzzlebobble/src/main/java/edu/uci/puzzlebobble/PuzzleBobbleViewController.java
@@ -1,11 +1,23 @@
 package edu.uci.puzzlebobble;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 import java.io.IOException;
 
-public class PuzzleBobbleViewController extends AnchorPane {
+public class PuzzleBobbleViewController extends StackPane {
+
+    @FXML private Circle nextTile;
+    @FXML private Circle currentTile;
+    @FXML private Line shooterLine;
+    @FXML private Label score;
+
     public PuzzleBobbleViewController() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/PuzzleBobbleView.fxml"));
         fxmlLoader.setRoot(this);
@@ -15,6 +27,19 @@ public class PuzzleBobbleViewController extends AnchorPane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+
+        registerMouseListeners();
+    }
+
+    private void registerMouseListeners() {
+        setOnMouseMoved(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                shooterLine.setStartX(event.getSceneX());
+                shooterLine.setStartY(event.getSceneY());
+                event.consume();
+            }
+        });
     }
 
 }

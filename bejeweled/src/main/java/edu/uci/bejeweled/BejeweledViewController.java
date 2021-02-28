74e0c25613ccb0f07d406ef7d4be4901155613f9
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-02-28 15:19:03 -0800

    Added logic to initialize tile grid of BejeweledView

--- bejeweled/src/main/java/edu/uci/bejeweled/BejeweledViewController.java
+++ bejeweled/src/main/java/edu/uci/bejeweled/BejeweledViewController.java
@@ -1,12 +1,21 @@
 package edu.uci.bejeweled;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
 
 import java.io.IOException;
 
 public class BejeweledViewController extends HBox {
+
+  @FXML private GridPane tileGrid;
+  @FXML private Label playerLabel;
+  @FXML private Label scoreLabel;
 
   public BejeweledViewController() {
     FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/BejeweledView.fxml"));
@@ -16,6 +25,21 @@ public class BejeweledViewController extends HBox {
       fxmlLoader.load();
     } catch (IOException exception) {
       throw new RuntimeException(exception);
+    }
+
+    initializeTileGrid();
+  }
+
+  private void initializeTileGrid() {
+    tileGrid.getRowConstraints().clear();
+    tileGrid.getColumnConstraints().clear();
+
+    // TODO get num rows/cols from board
+    for (int i = 0; i < 8; ++i) {
+      final RowConstraints row = new RowConstraints(65);
+      final ColumnConstraints col = new ColumnConstraints(65);
+      tileGrid.getRowConstraints().add(row);
+      tileGrid.getColumnConstraints().add(col);
     }
   }
 

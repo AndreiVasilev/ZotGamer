3a4cdd25f9f2523202d4ba51254b28ec71f3c004
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-02-28 16:11:05 -0800

    Added logic to BejeweledViewController to initialize tiles based on provided board.

--- bejeweled/src/main/java/edu/uci/bejeweled/BejeweledViewController.java
+++ bejeweled/src/main/java/edu/uci/bejeweled/BejeweledViewController.java
@@ -2,6 +2,7 @@ package edu.uci.bejeweled;
 
 
 import edu.uci.tmge.Pausable;
+import edu.uci.tmge.Tile;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
@@ -11,14 +12,16 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
 
 import java.io.IOException;
+import java.util.Collection;
 
 public class BejeweledViewController extends HBox implements Pausable {
 
   @FXML private GridPane tileGrid;
   @FXML private Label playerLabel;
   @FXML private Label scoreLabel;
+  private final BejeweledBoard bejeweledBoard;
 
-  public BejeweledViewController() {
+  public BejeweledViewController(final BejeweledBoard bejeweledBoard) {
     FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/BejeweledView.fxml"));
     fxmlLoader.setRoot(this);
     fxmlLoader.setController(this);
@@ -29,18 +32,27 @@ public class BejeweledViewController extends HBox implements Pausable {
     }
 
     initializeTileGrid();
+    this.bejeweledBoard = bejeweledBoard;
   }
 
   private void initializeTileGrid() {
     tileGrid.getRowConstraints().clear();
     tileGrid.getColumnConstraints().clear();
 
-    // TODO get num rows/cols from board
-    for (int i = 0; i < 8; ++i) {
+    for (int i = 0; i < bejeweledBoard.getHeight(); ++i) {
       final RowConstraints row = new RowConstraints(65);
-      final ColumnConstraints col = new ColumnConstraints(65);
       tileGrid.getRowConstraints().add(row);
+    }
+
+    for (int i = 0; i < bejeweledBoard.getWidth(); ++i) {
+      final ColumnConstraints col = new ColumnConstraints(65);
       tileGrid.getColumnConstraints().add(col);
+    }
+
+    final Collection<Tile> tiles = bejeweledBoard.getTiles();
+    for (final Tile tile : tiles) {
+      final BejeweledTile bejeweledTile = new BejeweledTile(tile);
+      tileGrid.add(bejeweledTile, (int) bejeweledTile.getXPos(), (int) bejeweledTile.getYPos());
     }
   }
 

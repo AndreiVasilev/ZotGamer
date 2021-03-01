b743ec2e23dd34678a77ecf1c8df2644004da5b6
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-02-28 16:10:43 -0800

    Replaced logic in MainViewController to call Bejeweled game instead of launching window itself

--- main/src/main/java/edu/uci/main/MainViewController.java
+++ main/src/main/java/edu/uci/main/MainViewController.java
@@ -1,6 +1,6 @@
 package edu.uci.main;
 
-import edu.uci.bejeweled.BejeweledViewController;
+import edu.uci.bejeweled.Bejeweled;
 import edu.uci.puzzlebobble.PuzzleBobbleViewController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -24,12 +24,8 @@ public class MainViewController extends AnchorPane {
 
   // TODO replace with Game Launcher
   public void playBejeweled() {
-    final Stage stage = new Stage();
-    final Scene mainScene = new Scene(new BejeweledViewController());
-    stage.setTitle("Bejeweled - Player 1");
-    stage.setScene(mainScene);
-    stage.setResizable(false);
-    stage.show();
+    final Bejeweled bejeweled = new Bejeweled();
+    bejeweled.launch();
   }
 
   // TODO replace with Game Launcher

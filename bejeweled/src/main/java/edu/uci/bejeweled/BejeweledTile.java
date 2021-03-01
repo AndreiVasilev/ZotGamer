b839245764c34c9c430bbe7fb5e058ff072ae10c
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-02-28 16:20:25 -0800

    Added borders to BejeweledTile and fixed color index error.

--- bejeweled/src/main/java/edu/uci/bejeweled/BejeweledTile.java
+++ bejeweled/src/main/java/edu/uci/bejeweled/BejeweledTile.java
@@ -7,22 +7,24 @@ import javafx.scene.shape.Rectangle;
 public class BejeweledTile extends Rectangle {
 
   private static final Color[] COLORS = {
-      Color.BLUE,
-      Color.RED,
+      Color.WHITE,
       Color.YELLOW,
-      Color.TEAL,
-      Color.PINK,
-      Color.GREEN,
-      Color.WHITE
+      Color.AQUA,
+      Color.HOTPINK,
+      Color.LIGHTGREEN,
+      Color.INDIANRED,
+      Color.MEDIUMPURPLE
   };
 
   private final Tile tile;
 
   public BejeweledTile(final Tile tile) {
     this.tile = tile;
-    setFill(COLORS[tile.getType()]);
+    setFill(COLORS[tile.getType() - 1]);
     setHeight(65.0);
     setWidth(65.0);
+    setStroke(Color.BLACK);
+    setStrokeWidth(3.0);
   }
 
   public double getXPos() {

6113724511c5eb65feda3dc93db79e5695653800
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-03-01 19:14:04 -0800

    Added logic to pause, resume, and quit PuzzleBobble

--- puzzlebobble/src/main/java/edu/uci/puzzlebobble/PuzzleBobble.java
+++ puzzlebobble/src/main/java/edu/uci/puzzlebobble/PuzzleBobble.java
@@ -6,46 +6,49 @@ import javafx.stage.Stage;
 
 public class PuzzleBobble implements Game {
 
+  private final PuzzleBobbleViewController viewController;
   private final PuzzleBobbleBoard board;
+  private final Stage gameWindow;
   private final int player;
 
   public PuzzleBobble(final int player) {
-    board = new PuzzleBobbleBoard();
     this.player = player;
+    board = new PuzzleBobbleBoard();
+    viewController = new PuzzleBobbleViewController(board);
+    gameWindow = new Stage();
+    gameWindow.setResizable(false);
+    gameWindow.setTitle("Puzzle Bobble - Player " + player);
   }
 
   @Override
   public void launch() {
-    final Stage stage = new Stage();
-    final Scene mainScene = new Scene(new PuzzleBobbleViewController(board));
-    stage.setResizable(false);
-    stage.setTitle("Puzzle Bobble - Player " + player);
-    stage.setScene(mainScene);
-    stage.show();
+    final Scene mainScene = new Scene(viewController);
+    gameWindow.setScene(mainScene);
+    gameWindow.show();
   }
 
   @Override
   public void pause() {
-
+    viewController.pause();
   }
 
   @Override
   public void resume() {
-
+    viewController.resume();
   }
 
   @Override
   public void quit() {
-
+    gameWindow.close();
   }
 
   @Override
   public String getName() {
-    return "";
+    return "Puzzle Bobble";
   }
 
   @Override
   public double getScore() {
-    return 0;
+    return board.getScore();
   }
 }

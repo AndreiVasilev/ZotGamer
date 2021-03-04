66fd21b39bc772d5935f78f53ac16d0e45861076
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-03-04 15:40:26 -0800

    Updated factories to return concrete types

--- bejeweled/src/main/java/edu/uci/bejeweled/BejeweledFactory.java
+++ bejeweled/src/main/java/edu/uci/bejeweled/BejeweledFactory.java
@@ -1,11 +1,10 @@
 package edu.uci.bejeweled;
 
-import edu.uci.tmge.Game;
 import edu.uci.tmge.GameFactory;
 
 public class BejeweledFactory implements GameFactory {
   @Override
-  public Game create(String playerName) {
+  public Bejeweled create(String playerName) {
     return new Bejeweled(playerName);
   }
 }
--- puzzlebobble/src/main/java/edu/uci/puzzlebobble/PuzzleBobbleFactory.java
+++ puzzlebobble/src/main/java/edu/uci/puzzlebobble/PuzzleBobbleFactory.java
@@ -1,11 +1,10 @@
 package edu.uci.puzzlebobble;
 
-import edu.uci.tmge.Game;
 import edu.uci.tmge.GameFactory;
 
 public class PuzzleBobbleFactory implements GameFactory {
   @Override
-  public Game create(String playerName) {
+  public PuzzleBobble create(String playerName) {
     return new PuzzleBobble(playerName);
   }
 }
